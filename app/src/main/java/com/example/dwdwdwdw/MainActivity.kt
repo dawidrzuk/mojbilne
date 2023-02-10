beaa1e560385e2a1330829c5a8e775c703ffad6b
OGMaksiu <mateckimaksymilian@gmail.com>
2023-02-10 15:51:12 +0100

    layout + obrazki

--- app/src/main/java/com/example/dwdwdwdw/MainActivity.kt
+++ app/src/main/java/com/example/dwdwdwdw/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dwdwdwdw
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,12 @@ class MainActivity : AppCompatActivity() {
         val obraz1 = findViewById<ImageView>(R.id.imageView)
         val obraz2 = findViewById<ImageView>(R.id.imageView2)
         val obraz3 = findViewById<ImageView>(R.id.imageView3)
+        val sb1szer = findViewById<SeekBar>(R.id.seekBar)
+        val sb2szer = findViewById<SeekBar>(R.id.seekBar2)
+        val sb3szer = findViewById<SeekBar>(R.id.seekBar3)
+        val sb1dlug = findViewById<SeekBar>(R.id.seekBar5)
+        val sb2dlug = findViewById<SeekBar>(R.id.seekBar6)
+        val sb3dlug = findViewById<SeekBar>(R.id.seekBar7)
+
     }
 }
